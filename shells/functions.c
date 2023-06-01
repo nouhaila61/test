5a48498922509219ef3156882b34eac987a8bf32
nouhaila61 <nouhailaelbouhali99@gmail.com>
2023-06-01 18:16:34 +0100

    test

--- shells/functions.c
+++ shells/functions.c
@@ -5,73 +5,73 @@
 
 int lookforslash(char *cmd)
 {
-    int counter = 0;
+	int counter = 0;
 
-    while (cmd[counter])
-    {
-        if (cmd[0] == '/')
-        {
-            printf("%c\n", cmd[0]);
-            return 1;
-        }
+	while (cmd[counter])
+	{
+		if (cmd[0] == '/')
+		{
+			printf("%c\n", cmd[0]);
+			return 1;
+		}
 
-        counter++;
-    }
-    return 0;
+		counter++;
+	}
+	return 0;
 }
 
 int compareExit(char *s1, char *s2)
 {
-    int i = 0;
+	int i = 0;
 
-    for (; (*s2 != '\0' && *s1 != '\0') && *s1 == *s2; s1++)
-    {
-        if (i == 3)
-            break;
-        i++;
-        s2++;
-    }
+	for (; (*s2 != '\0' && *s1 != '\0') && *s1 == *s2; s1++)
+	{
+		if (i == 3)
+			break;
+		i++;
+		s2++;
+	}
 
-    return (*s1 - *s2);
+	return (*s1 - *s2);
 }
 
 int compareEnv(char *s1, char *s2)
 {
-    int i = 0;
+	int i = 0;
 
-    for (; (*s2 != '\0' && *s1 != '\0') && *s1 == *s2; s1++)
-    {
-        if (i == 2)
-            break;
-        i++;
-        s2++;
-    }
+	for (; (*s2 != '\0' && *s1 != '\0') && *s1 == *s2; s1++)
+	{
+		if (i == 2)
+			break;
+		i++;
+		s2++;
+	}
 
-    return (*s1 - *s2);
+	return (*s1 - *s2);
 }
 
 char **identify_string(char *parameter)
 {
-    char **buf = malloc(1024 * sizeof(char *));
-    char *split;
-    int i = 0;
-    char *delim = " \t\n";
+	char **buf = malloc(1024 * sizeof(char *));
+	char *split;
+	int i = 0;
+	char *delim = " \t\n";
 
-    split = strtok(parameter, delim);
+	split = strtok(parameter, delim);
 
-    while (split != NULL)
-    {
-        buf[i] = split;
-        i++;
-        split = strtok(NULL, delim);
-    }
-    buf[i] = NULL;
-    execute_proc(buf);
-    return buf;
+	while (split != NULL)
+	{
+		buf[i] = split;
+		i++;
+		split = strtok(NULL, delim);
+	}
+	buf[i] = NULL;
+	execute_proc(buf);
+	return buf;
 }
 
 void counterrolC(int sig)
 {
-    (void)sig;
-    write(1, "\n$ ", 3);
+	(void)sig;
+	write(1, "\n$ ", 3);
 }
